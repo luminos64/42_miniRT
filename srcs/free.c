06023d8c2715e4bc0795009e56a9c6f474a10a8b
Nonthakorn <nnonthakornn@gmail.com>
2025-05-12 16:15:14 +0700

    Check NULL before free

--- srcs/free.c
+++ srcs/free.c
@@ -11,12 +11,20 @@ void	ft_free_exit(char **split_line, t_data *id, int fd, char *err_msg)
 
 void	free_success(t_data *id)
 {
-	free(id->ambient);
-	free(id->camera);
-	free_light(id->light);
-	free_plane(id->plane);
-	free_sphere(id->shape);
-	free_cylin(id->cylynder);
+	if (!id)
+		return ;
+	if (id->ambient)
+		free(id->ambient);
+	if (id->camera)
+		free(id->camera);
+	if (id->light)
+		free_light(id->light);
+	if (id->plane)
+		free_plane(id->plane);
+	if (id->shape)
+		free_sphere(id->shape);
+	if (id->cylynder)
+		free_cylin(id->cylynder);
 }
 
 void	free_light(t_light *light_node)
@@ -24,6 +32,8 @@ void	free_light(t_light *light_node)
 	t_light	*curr;
 	t_light	*next_node;
 
+	if (!light_node)
+		return ;
 	curr = light_node;
 	while (curr)
 	{
--- srcs/free_shape.c
+++ srcs/free_shape.c
@@ -5,6 +5,8 @@ void	free_plane(t_plane *plane_node)
 	t_plane	*curr;
 	t_plane	*next_node;
 
+	if (!plane_node)
+		return ;
 	curr = plane_node;
 	while (curr)
 	{
@@ -19,6 +21,8 @@ void	free_sphere(t_sphere *sphere_node)
 	t_sphere	*curr;
 	t_sphere	*next_node;
 
+	if (!sphere_node)
+		return ;
 	curr = sphere_node;
 	while (curr)
 	{
@@ -33,6 +37,8 @@ void	free_cylin(t_cylinder *cylin_node)
 	t_cylinder	*curr;
 	t_cylinder	*next_node;
 
+	if (!cylin_node)
+		return ;
 	curr = cylin_node;
 	while (curr)
 	{
